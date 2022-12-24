b73e034c1baab8c97d168d3b18ccf6acc2f12e34
HamzahRaihan <hamzahraihanif@gmail.com>
2022-12-24 21:22:05 +0700

    Learn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import { planets } from "./Planet";
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import "bootstrap/dist/js/bootstrap.bundle";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="name">Hamzah</h1>
+      {planets.map((planet, key) => {
+        if (planet.isGasPlanet) return <h1>{planet.name}</h1>;
+      })}
     </div>
   );
 }
---
+++ src/tryBootstrap.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+
+function App() {
+  return (
+    <div className="Navbar">
+      <nav className="navbar bg-light">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1">Navbar</span>
+        </div>
+      </nav>
+    </div>
+  );
+}
