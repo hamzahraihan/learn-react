5b07598a4713c587324e324b86f352e53598554f
HamzahRaihan <hamzahraihanif@gmail.com>
2022-12-26 20:45:33 +0700

    Learn react router dom

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import "./App.css";
-import Axios from "axios";
-import { useEffect, useState } from "react";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import "bootstrap/dist/js/bootstrap.bundle";
 
 function App() {
-  const [generateExcuse, setGenerateExcuse] = useState("");
-
-  const fetchExcuse = (excuse) => {
-    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then((res) => {
-      setGenerateExcuse(res.data[0].excuse);
-    });
-  };
   return (
     <div className="App">
-      <h1>Generate an Excuse</h1>
-      <button onClick={() => fetchExcuse("party")}>Party</button>
-      <button onClick={() => fetchExcuse("family")}>Family</button>
-      <button onClick={() => fetchExcuse("office")}>Office</button>
-      <br />
-      <p>{generateExcuse}</p>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+      </Routes>
     </div>
   );
 }
